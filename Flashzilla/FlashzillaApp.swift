fdd730f95616e5014788f6b659a0bf4a29d63d16
PRABAL4546 <prabaljitwalia4546@gmail.com>
2021-02-09 11:32:49 +0530

    first commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by PRABALJIT WALIA     on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
